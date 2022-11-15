79f37aa877699a20a8cf250688b32be287360c38
Ryan Williams <r.d.williams.ca@gmail.com>
2022-11-15 15:04:23 -0800

    forgot to add response route to server, added now

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/prompts", require("./routes/promptsRoutes"));
 app.use("/api/moods", require("./routes/moodsRoutes"));
+app.use("/api/responses", require("./routes/responseRoutes"));
 
 app.use(errorHandler);
 
